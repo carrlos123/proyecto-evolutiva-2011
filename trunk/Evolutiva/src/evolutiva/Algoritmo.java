764fed8f05cba98fde9c1bf2f511166ce5a0c72b
christianqw <christianqw@4d757058-bb42-080a-9c5d-a9cf42a2c55c>
2011-11-05 21:01:07 +0000

    con la clase Algoritmo agregada

--- trunk/Evolutiva/src/evolutiva/Algoritmo.java
+++ trunk/Evolutiva/src/evolutiva/Algoritmo.java
@@ -1,71 +1,119 @@
 /*
- * EvolutivaApp.java
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
+
 
 package evolutiva;
 
+import evolutiva.operadores.CruceDeCiclos;
+import evolutiva.operadores.CruceDeOrden;
+import evolutiva.operadores.MutacionPorInsercion;
+import evolutiva.operadores.MutacionPorIntercambio;
+import evolutiva.operadores.MutacionPorInversion;
+import evolutiva.operadores.OperadorCruce;
+import evolutiva.operadores.OperadorMutacion;
 import org.jdesktop.application.Application;
 import org.jdesktop.application.SingleFrameApplication;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Vector;
+import evolutiva.seleccionadores.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
- * The main class of the application.
+ *
+ * @author Cristian
  */
-public class EvolutivaApp extends SingleFrameApplication {
-/*
-    private static final int COMPETIDORES = 4;
+
+public class Algoritmo {
+    private static int COMPETIDORES = 4;
     //cantidad de competidores en la seleccion por torneo, para seleccionar padres.
-    private static final int TAM_POBLACION = 100;
+    private static int TAM_POBLACION = 100;
     //cantidad de individuos en la poblacion.
-    private static final float PORCENTAJE_SOBREVIVIENTES = 0.2f;
+    private static float PORCENTAJE_SOBREVIVIENTES = 0.2f;
     //% de sobreviviente de la vieja generacion.
-    private static final double PROB_CRUCE = 1.0f;
+    private static double PROB_CRUCE = 1.0f;
     //probabilidad de realizar un cruce entre 2 individuos seleccionados como padres.
-    private static final double PROB_MUTAR = 0.5f;
+    private static double PROB_MUTAR = 0.5f;
     //probabilidad de mutar un individuo despues de su nacimiento.
-*/    
+
+    //----COMPONENTES:
+    FuncionFitness f;
+    GeneradorPoblacion g;
+    SeleccionPadres s;
+    OperadorMutacion m;
+    OperadorCruce c;
+    SeleccionSobrevivientes vivos;
+        
     
-    /**
-     * At startup create and show the main frame of the application.
-     */
-    @Override protected void startup() {
-        show(new EvolutivaView(this));
+    public Algoritmo() {
+        this.f = new FitnessConHistorial();//Inicializamos la funcion de Fitness Seleccionada.
+        Individuo.setCalculadorFitness(f);//Asignmos la funcion de Fitness dentro de los Individuos. 
+        
+        this.g= new GPConjunto();//Inicializamos el Generador Poblacional Seleccionado.
+        Poblacion p;
+        
+        this.s = new PadresPorTorneo(COMPETIDORES);
+        this.m = new MutacionPorIntercambio(PROB_MUTAR);
+        this.c = new CruceDeCiclos(PROB_CRUCE);
+        this.vivos = new SobrevivientesStadyState(PORCENTAJE_SOBREVIVIENTES);
+        
+        
     }
 
-    /**
-     * This method is to initialize the specified window by injecting resources.
-     * Windows shown in our application come fully initialized from the GUI
-     * builder, so this additional configuration is not needed.
-     */
-    @Override protected void configureWindow(java.awt.Window root) {
+    public void setCruce(OperadorCruce c) {
+        this.c = c;
     }
 
-    /**
-     * A convenient static getter for the application instance.
-     * @return the instance of EvolutivaApp
-     */
-    public static EvolutivaApp getApplication() {
-        return Application.getInstance(EvolutivaApp.class);
+    public void setFuncionFitnes(FuncionFitness f) {
+        this.f = f;
     }
 
-     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public void setGeneradorPobalcional(GeneradorPoblacion g) {
+        this.g = g;
+    }
+
+    public void setMutacion(OperadorMutacion m) {
+        this.m = m;
+    }
+
+    public void setSeleccionadorPadres(SeleccionPadres s) {
+        this.s = s;
+    }
+
+    public void setSobrevivientes(SeleccionSobrevivientes vivos) {
+        this.vivos = vivos;
+    }
+
     
-    /**
-     * Main method launching the application.
-     */
+    public static void setCompetidores(int COMPETIDORES) {
+        Algoritmo.COMPETIDORES = COMPETIDORES;
+    }
+
+    public static void setPorsentajeSobrevivientes(float PORCENTAJE_SOBREVIVIENTES) {
+        Algoritmo.PORCENTAJE_SOBREVIVIENTES = PORCENTAJE_SOBREVIVIENTES;
+    }
+
+    public static void setProbCruce (double PROB_CRUCE) {
+        Algoritmo.PROB_CRUCE = PROB_CRUCE;
+    }
+
+    public static void setProbMutar(double PROB_MUTAR) {
+        Algoritmo.PROB_MUTAR = PROB_MUTAR;
+    }
+
+    public static void setTamPoblacion (int TAM_POBLACION) {
+        Algoritmo.TAM_POBLACION = TAM_POBLACION;
+    }
     
-    public static void main(String[] args) throws IOException{
-        launch(EvolutivaApp.class, args);
-        
-        Algoritmo a = new Algoritmo();
-        a.ejecutar();
-        
-        /*
-        
-        // TODO code application logic here
+    public void ejecutar() throws IOException{
+    
         String ACC = "..\\instanciaN120G20\\cantColores.txt";
         String ACG ="..\\instanciaN120G20\\cantGrupos.txt";
         String ACN ="..\\instanciaN120G20\\cantNaipes.txt";
@@ -79,36 +127,9 @@ public class EvolutivaApp extends SingleFrameApplication {
         Info.AbrirArchivoColores(AC);
         Info.AbrirArchivoNaipes(AN);
 
-        FuncionFitness f = new FitnessConHistorial();//Inicializamos la funcion de Fitness Seleccionada.
-        Individuo.setCalculadorFitness(f);//Asignmos la funcion de Fitness dentro de los Individuos. 
-        
         GeneradorPoblacion g= new GPConjunto();//Inicializamos el Generador Poblacional Seleccionado.
         Poblacion p;
        
-    //    p.mostrame();  //mostramo la lista total de individuos con su fitness asociado.
-        
-        SeleccionPadres s = new PadresPorTorneo(COMPETIDORES);
-        OperadorMutacion m = new MutacionPorIntercambio(PROB_MUTAR);
-        OperadorCruce c = new CruceDeCiclos(PROB_CRUCE);
-        SeleccionSobrevivientes vivos = new SobrevivientesStadyState(PORCENTAJE_SOBREVIVIENTES);
-        
- /*       
-        **** * PARTE PARA TESTEAR OPERADORES DE CRUCE Y MUTACION***** 
-         * descomentar esto y comentar todo lo de abajo * /
-        Individuo p1 = s.getPadre(p);
-        System.out.println("p1 : \n " + p1.toString());
-        Individuo p2 = s.getPadre(p);
-        System.out.println("p2 : \n " + p2.toString());
-        
-        
-        System.out.println("Comienza mutacion de padre1");
-        System.out.println(m.mutar(p1).toString());
-        
-        System.out.println("El mejor fitness lo tine: \n" + p.getMejorIndividuo());
-        System.out.println("----- POBLACION ORDENADA -----" );
-        p.mostrarIndividuos();   
-        */
-/*
         System.out.println("xxxxxxx   PROBANDO EJECUCION GENERICA   xxxxxxx" );
         for(int vueltas = 0; vueltas < 20; vueltas++){
             p = g.generar(TAM_POBLACION);
@@ -151,6 +172,8 @@ public class EvolutivaApp extends SingleFrameApplication {
             long tiempoTotal = tiempoFin.getTimeInMillis() - tiempo0.getTimeInMillis();
             System.out.println("tiempo de ejecucion: " + tiempoTotal);
             System.out.println("ejecucion numero: " + vueltas);
-        }*/
+        }
+    
+        
     }
 }
